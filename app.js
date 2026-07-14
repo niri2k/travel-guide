3bd44a6a7b9ad9d3937b36936a7cbd71dce4a264
niri2k <88641668+niri2k@users.noreply.github.com>
2026-07-15 00:35:06 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,103 @@
 
+let trips=[];
 
-fetch("data/travel-guide.json")
 
-.then(response => response.json())
+
+fetch("data/trips.json")
+
+.then(res=>res.json())
 
 .then(data=>{
+
+
+trips=data;
+
+
+showTrips();
+
+
+});
+
+
+
+
+function showTrips(){
+
+
+let html="";
+
+
+trips.forEach(trip=>{
+
+
+html += `
+
+
+<div class="trip"
+onclick="openTrip('${trip.file}')">
+
+
+<h3>
+
+${trip.title}
+
+</h3>
+
+
+<p>
+
+${trip.date}
+
+</p>
+
+
+<p>
+
+${trip.status}
+
+</p>
+
+
+</div>
+
+
+`;
+
+});
+
+
+document.getElementById(
+"tripList"
+).innerHTML=html;
+
+
+}
+
+
+
+
+function openTrip(file){
+
+
+fetch("trips/"+file)
+
+.then(res=>res.json())
+
+.then(data=>{
+
+
+document
+.getElementById("tripDetail")
+.classList
+.remove("hidden");
+
+
+
+document
+.getElementById("tripTitle")
+.innerHTML=
+data.title;
+
 
 
 let html="";
@@ -15,11 +108,15 @@ data.days.forEach(day=>{
 
 html += `
 
+
 <div class="day">
+
 
 <h3>
 
 DAY ${day.day}
+
+<br>
 
 ${day.title}
 
@@ -35,49 +132,19 @@ ${day.description}
 
 </div>
 
+
 `;
 
 });
 
 
-document.getElementById(
-"schedule"
-).innerHTML=html;
+
+document
+.getElementById("schedule")
+.innerHTML=html;
 
 
 });
-
-
-
-function saveExpense(){
-
-
-let money =
-document.getElementById("money").value;
-
-
-let memo =
-document.getElementById("memo").value;
-
-
-localStorage.setItem(
-
-"expense",
-
-JSON.stringify({
-
-money,
-
-memo
-
-})
-
-);
-
-
-alert(
-"저장되었습니다"
-);
 
 
 }
