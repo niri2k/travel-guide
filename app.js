3d34e2e65221bbf0c6c5dd6f641cf209f80ac90b
niri2k <88641668+niri2k@users.noreply.github.com>
2026-07-15 00:18:10 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,83 @@
 
+
+fetch("data/xian2026.json")
+
+.then(response => response.json())
+
+.then(data=>{
+
+
+let html="";
+
+
+data.days.forEach(day=>{
+
+
+html += `
+
+<div class="day">
+
+<h3>
+
+DAY ${day.day}
+
+${day.title}
+
+</h3>
+
+
+<p>
+
+${day.description}
+
+</p>
+
+
+</div>
+
+`;
+
+});
+
+
+document.getElementById(
+"schedule"
+).innerHTML=html;
+
+
+});
+
+
+
+function saveExpense(){
+
+
+let money =
+document.getElementById("money").value;
+
+
+let memo =
+document.getElementById("memo").value;
+
+
+localStorage.setItem(
+
+"expense",
+
+JSON.stringify({
+
+money,
+
+memo
+
+})
+
+);
+
+
+alert(
+"저장되었습니다"
+);
+
+
+}
