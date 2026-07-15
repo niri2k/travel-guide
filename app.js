4604b33b5c2514839e870c409872df1ea416d53b
niri2k <88641668+niri2k@users.noreply.github.com>
2026-07-15 10:54:47 +0900

    Update app.js
    오류로 인해 전체 소스 복붙

--- app.js
+++ app.js
@@ -1,71 +1,109 @@
+let trips = [];
 
-let currentTrip="";
-let currentCurrency="CNY";
-let exchangeRate=0;
-let trips=[];
+let currentTrip = "";
 
+let currentCurrency = "";
 
-fetch("data/trips.json")
-.then(res=>res.json())
-.then(data=>{
+let exchangeRate = 0;
 
-
-trips=data;
+let expenses = [];
 
 
-showTrips();
 
-
-});
 
 
 function getToday(){
 
-    let today = new Date();
-
-    return today.toISOString()
+    return new Date()
+    .toISOString()
     .substring(0,10);
 
 }
 
 
+
+
+
+
+fetch("data/trips.json")
+
+.then(response=>response.json())
+
+.then(data=>{
+
+
+    trips=data;
+
+    showTrips();
+
+
+})
+
+.catch(error=>{
+
+
+document.getElementById(
+"tripList"
+)
+.innerHTML=
+
+"❌ 여행 데이터를 불러오지 못했습니다.";
+
+
+});
+
+
+
+
+
+
+
 function showTrips(){
 
 
-let categories = {
+let categories={
 
-    upcoming:{
-        title:"✈️ 다가올 여행",
-        items:[]
-    },
 
-    active:{
-        title:"🌏 여행 중",
-        items:[]
-    },
+upcoming:{
+title:"✈️ 다가올 여행",
+items:[]
+},
 
-    memory:{
-        title:"📸 여행의 기억",
-        items:[]
-    }
+
+active:{
+title:"🌏 여행 중",
+items:[]
+},
+
+
+memory:{
+title:"📸 여행의 기억",
+items:[]
+}
+
 
 };
+
 
 
 
 trips.forEach(trip=>{
 
 
-    if(categories[trip.status]){
+if(categories[trip.status]){
 
-        categories[trip.status]
-        .items
-        .push(trip);
 
-    }
+categories[trip.status]
+.items
+.push(trip);
+
+
+}
 
 
 });
+
+
 
 
 
@@ -77,149 +115,187 @@ Object.values(categories)
 .forEach(category=>{
 
 
-    html += `
+html += `
 
-    <h2>
-    ${category.title}
-    </h2>
+<h3>
+${category.title}
+</h3>
 
-    `;
-
-
-    if(category.items.length===0){
-
-        html += `
-        <p>
-        등록된 여행이 없습니다.
-        </p>
-        `;
-
-    }
+`;
 
 
 
-    category.items.forEach(trip=>{
-        html += `
-        <div class="trip"
-        onclick="openTrip('${trip.file}')">
+category.items.forEach(trip=>{
 
-        <h3>
-        ${trip.title}
-        </h3>
 
-        <p>
-        📅 ${trip.date}
-        </p>
+html += `
 
-        </div>
 
-        `;
+<div class="trip"
+onclick="openTrip('${trip.file}')">
 
-    });
+
+<h3>
+${trip.title}
+</h3>
+
+
+<p>
+📅 ${trip.date}
+</p>
+
+
+</div>
+
+
+`;
+
+
+});
+
 
 });
 
 
 
 document
-.getElementById("tripList")
+.getElementById(
+"tripList"
+)
 .innerHTML=html;
 
 
 }
 
 
+
+
+
+
+
+
+
 function openTrip(file){
 
+
 currentTrip=file;
-currentCurrency =
-data.currency;
 
-loadExchange();
-    
-expenses =
-JSON.parse(
-localStorage.getItem(
-"expenses_"+currentTrip
-)
-)
-|| [];
 
-showExpenses();
 
-document.getElementById(
-"expenseDate"
-).value = getToday();
-
-    
 fetch("trips/"+file)
-.then(res=>res.json())
+
+.then(response=>response.json())
+
 .then(data=>{
 
+
+
 document
-.getElementById("tripDetail")
+.getElementById(
+"tripDetail"
+)
 .classList
 .remove("hidden");
 
+
+
 document
-.getElementById("tripTitle")
+.getElementById(
+"tripTitle"
+)
 .innerHTML=
 data.title;
 
+
+
+currentCurrency =
+data.currency;
+
+
+
+loadExchange();
+
+
+
+
+document
+.getElementById(
+"expenseDate"
+)
+.value=getToday();
+
+
+
+
+
 let html="";
 
+
+
 data.days.forEach(day=>{
+
+
 html += `
+
+
 <div class="day">
+
+
 <h3>
+
 DAY ${day.day}
+
 <br>
+
 ${day.title}
+
 </h3>
+
 
 `;
 
 
+
 day.places.forEach(place=>{
+
 
 html += `
 
+
 <div class="place">
+
 
 <h4>
 📍 ${place.name}
 </h4>
 
+
 <p>
-💰 예상 비용:
-${place.cost} ${data.currency}
+
+💰 예상 비용 :
+${place.cost}
+${data.currency}
+
 </p>
 
-<button onclick="window.open('${place.map}')">
+
+
+<button
+onclick="window.open('${place.map}')">
 
 지도 보기
 
 </button>
 
 
-<p>
-
-⭐ ☆☆☆☆☆
-
-</p>
-
-
-<textarea
-placeholder="후기 작성">
-</textarea>
-
-
 </div>
+
 
 `;
 
 
 });
+
 
 
 html += `
@@ -229,166 +305,46 @@ html += `
 `;
 
 
-});
-
-
-document
-.getElementById("schedule")
-.innerHTML=html;
-
 
 });
 
 
-}
-
-
-let expenses =
-JSON.parse(
-localStorage.getItem("expenses")
-)
-|| [];
-
-
-function addExpense(){
-
-
-let date =
-document.getElementById(
-"expenseDate"
-).value;
-
-
-let amount =
-Number(
-document.getElementById(
-"expenseAmount"
-).value
-);
-
-
-let memo =
-document.getElementById(
-"expenseMemo"
-).value;
-
-
-expenses.push({
-date: date,
-amount: amount,
-memo: memo
-
-});
-
-
-
-localStorage.setItem(
-"expenses_"+currentTrip,
-JSON.stringify(expenses)
-
-);
-
-
-showExpenses();
-
-}
-
-
-function showExpenses(){
-
-
-let total=0;
-
-let totalKRW=0;
-    
-let html="";
-
-
-expenses.forEach(item=>{
-
-total += item.amount;
-
-totalKRW +=
-item.amount * exchangeRate;
-    
-html += `
-
-<div class="place">
-
-<p>
-${item.date}
-</p>
-
-<h4>
-
-${item.memo}
-
-</h4>
-
-<p>
-
-${item.amount} ${currentCurrency}
-
-<br>
-
-≈
-
-${Math.round(
-item.amount * exchangeRate
-)}
-
-원
-</p>
-
-
-</div>
-
-`;
-
-
-});
 
 
 document
 .getElementById(
-"totalExpense"
-)
-.innerHTML=
-
-total+" CNY";
-
-document
-.getElementById(
-"totalKRW"
-)
-.innerHTML =
-
-Math.round(totalKRW)
-.toLocaleString()
-+"원";
-
-document
-.getElementById(
-"expenseList"
+"schedule"
 )
 .innerHTML=html;
 
 
+
+loadExpenses();
+
+
+
+});
+
+
+
 }
 
 
 
-showExpenses();
+
+
+
+
 
 
 function loadExchange(){
 
 
-fetch(
-"data/exchange.json"
-)
+fetch("data/exchange.json")
 
-.then(res=>res.json())
+
+.then(response=>response.json())
+
 
 .then(data=>{
 
@@ -397,16 +353,18 @@ exchangeRate =
 data[currentCurrency];
 
 
+
 document
 .getElementById(
 "exchangeRate"
 )
-.innerHTML =
+.innerHTML=
 
 `
 💱 통화 : ${currentCurrency}
 /
-환율 : 1 ${currentCurrency} = ${exchangeRate}원
+환율 : 1 ${currentCurrency}
+= ${exchangeRate}원
 (2026-07-15 기준)
 
 `;
@@ -416,7 +374,224 @@ document
 showExpenses();
 
 
+
 });
+
+
+}
+
+
+
+
+
+
+
+
+function addExpense(){
+
+
+
+let date =
+document
+.getElementById(
+"expenseDate"
+)
+.value;
+
+
+
+let amount =
+Number(
+document
+.getElementById(
+"expenseAmount"
+)
+.value
+);
+
+
+
+let memo =
+document
+.getElementById(
+"expenseMemo"
+)
+.value;
+
+
+
+
+
+expenses.push({
+
+date,
+amount,
+memo
+
+});
+
+
+
+
+
+localStorage.setItem(
+
+"expense_"+currentTrip,
+
+JSON.stringify(expenses)
+
+);
+
+
+
+
+showExpenses();
+
+
+}
+
+
+
+
+
+
+
+
+
+function loadExpenses(){
+
+
+expenses =
+
+JSON.parse(
+
+localStorage.getItem(
+
+"expense_"+currentTrip
+
+)
+
+)
+
+|| [];
+
+
+
+showExpenses();
+
+
+}
+
+
+
+
+
+
+
+
+function showExpenses(){
+
+
+let total=0;
+
+let krw=0;
+
+let html="";
+
+
+
+
+expenses.forEach(item=>{
+
+
+total += item.amount;
+
+
+krw +=
+
+item.amount *
+exchangeRate;
+
+
+
+
+html += `
+
+
+<div class="place">
+
+
+<p>
+📅 ${item.date}
+</p>
+
+
+<h4>
+${item.memo}
+</h4>
+
+
+<p>
+
+${item.amount}
+${currentCurrency}
+
+<br>
+
+≈
+
+${Math.round(
+item.amount*exchangeRate
+)
+.toLocaleString()}
+원
+
+</p>
+
+
+</div>
+
+
+`;
+
+
+});
+
+
+
+
+
+document
+.getElementById(
+"totalExpense"
+)
+.innerHTML=
+
+`
+${total}
+${currentCurrency}
+`;
+
+
+
+document
+.getElementById(
+"totalKRW"
+)
+.innerHTML=
+
+`
+약 ${Math.round(krw)
+.toLocaleString()}원
+`;
+
+
+
+document
+.getElementById(
+"expenseList"
+)
+.innerHTML=html;
 
 
 }
