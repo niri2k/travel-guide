c8515ec29721e0f4724afcc8193ff177d24f3e8f
niri2k <88641668+niri2k@users.noreply.github.com>
2026-07-15 11:30:46 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 let trips = [];
 
 
+
 fetch("data/trips.json")
 
-.then(res => res.json())
+.then(response => {
+
+    if (!response.ok) {
+
+        throw new Error(
+            "여행 목록 파일을 불러올 수 없습니다."
+        );
+
+    }
+
+    return response.json();
+
+})
 
 .then(data => {
 
@@ -11,95 +24,185 @@ fetch("data/trips.json")
 
     showTrips();
 
+})
+
+.catch(error => {
+
+
+    document
+    .getElementById("tripList")
+    .innerHTML =
+
+    `
+    ❌ 오류 발생
+
+    <br>
+
+    ${error.message}
+    `;
+
+
 });
+
+
+
 
 
 
 function showTrips(){
 
-let categories = {
 
-upcoming:{
-title:"✈️ 다가올 여행",
-items:[]
-},
-
-active:{
-title:"🌏 여행 중",
-items:[]
-},
-
-memory:{
-title:"📸 여행의 기억",
-items:[]
-}
-
-};
+    let categories = {
 
 
-trips.forEach(trip=>{
+        upcoming: {
 
-if(categories[trip.status]){
+            title:"✈️ 다가올 여행",
 
-categories[trip.status]
-.items
-.push(trip);
+            items:[]
 
-}
-
-});
+        },
 
 
+        active: {
 
-let html="";
+            title:"🌏 여행 중",
 
+            items:[]
 
-Object.values(categories)
-.forEach(category=>{
-
-
-html += `
-
-<h3>
-${category.title}
-</h3>
-
-`;
+        },
 
 
-category.items.forEach(trip=>{
+        memory: {
+
+            title:"📸 여행의 기억",
+
+            items:[]
+
+        }
 
 
-html += `
-
-<div class="trip"
-
-onclick="openTrip('${trip.file}')">
+    };
 
 
-<h3>
-${trip.title}
-</h3>
 
 
-<p>
-📅 ${trip.date}
-</p>
+
+    trips.forEach(trip => {
 
 
-</div>
-
-`;
-
-});
+        if(categories[trip.status]) {
 
 
-});
+            categories[trip.status]
+            .items
+            .push(trip);
 
 
-document
-.getElementById("tripList")
-.innerHTML=html;
+        }
+
+
+    });
+
+
+
+
+
+    let html = "";
+
+
+
+
+
+    Object.values(categories)
+
+    .forEach(category => {
+
+
+
+        html += `
+
+        <h3>
+        ${category.title}
+        </h3>
+
+        `;
+
+
+
+
+
+        if(category.items.length === 0){
+
+
+            html += `
+
+            <p>
+            등록된 여행이 없습니다.
+            </p>
+
+            `;
+
+
+        }
+
+
+
+
+
+
+
+        category.items.forEach(trip => {
+
+
+
+            html += `
+
+
+            <div class="trip"
+
+            onclick="openTrip('${trip.file}')">
+
+
+                <h3>
+
+                ${trip.title}
+
+                </h3>
+
+
+                <p>
+
+                📅 ${trip.date}
+
+                </p>
+
+
+            </div>
+
+
+            `;
+
+
+
+        });
+
+
+
+    });
+
+
+
+
+
+
+
+    document
+
+    .getElementById("tripList")
+
+    .innerHTML = html;
+
 
 
 }
