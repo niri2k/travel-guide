0f4f7caa002aa7d0236f2ff8290d1a4a9406ac86
niri2k <88641668+niri2k@users.noreply.github.com>
2026-07-15 11:24:56 +0900

    Update exchange.js

--- js/exchange.js
+++ js/exchange.js
@@ -1,1 +1,38 @@
+let exchangeRate=0;
 
+
+function loadExchange(){
+
+
+fetch("data/exchange.json")
+
+.then(res=>res.json())
+
+.then(data=>{
+
+
+exchangeRate =
+data[currentCurrency];
+
+
+document
+.getElementById("exchangeRate")
+.innerHTML=
+
+`
+💱 통화 : ${currentCurrency}
+/
+환율 : 1 ${currentCurrency}
+= ${exchangeRate}원
+(2026-07-15 기준)
+
+`;
+
+
+showExpenses();
+
+
+});
+
+
+}
