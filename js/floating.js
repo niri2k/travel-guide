e15ab6b4e85958430d3013fbe77e44f7bef0ff72
niri2k <88641668+niri2k@users.noreply.github.com>
2026-07-15 11:56:24 +0900

    Update floating.js

--- js/floating.js
+++ js/floating.js
@@ -1,1 +1,76 @@
+function toggleFab(){
 
+
+let menu =
+document.getElementById(
+"fabMenu"
+);
+
+
+menu.classList.toggle(
+"hidden"
+);
+
+
+}
+
+
+
+
+function openExpenseForm(){
+
+
+toggleFab();
+
+
+document
+.getElementById(
+"expenseAmount"
+)
+.scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+
+}
+
+
+
+
+function openScheduleForm(){
+
+
+alert(
+"일정 추가 기능 준비 중입니다."
+);
+
+
+}
+
+
+
+
+function openPlaceForm(){
+
+
+alert(
+"방문 장소 기록 기능 준비 중입니다."
+);
+
+
+}
+
+
+
+
+function openReviewForm(){
+
+
+alert(
+"후기 작성 기능 준비 중입니다."
+);
+
+
+}
